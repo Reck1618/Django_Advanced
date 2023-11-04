e4b215a98570680de739b3d18b4a587fbdc1809c
Ritik <ritiktyagi1.618@gmail.com>
2023-11-04 20:14:26 +0530

    blog app

---
+++ blango_blog/blog/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import get_object_or_404, redirect, render
+from blog.models import Post
+from blog.forms import CommentForm
+from django.utils import timezone
+import logging
+
+logger = logging.getLogger(__name__)
+
+# Create your views here.
+
+def index(request):
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.info(f"Posts:{len(posts)}")
+    return render(request, 'blog/index.html', {'posts': posts})
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+
+    if request.user.is_active:
+        if request.method == 'POST':
+            comment_form = CommentForm(request.POST)
+
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+
+                comment.creator = request.user
+                comment.content_object = post
+                comment.save()
+                logger.info(f"Created comment on Post {post.pk} by {request.user}")
+                return redirect(request.path_info)
+        else:
+            comment_form = CommentForm()
+    else:
+        comment_form = None
+
+    return render(request, 'blog/post-detail.html', {'post': post, 'comment_form': comment_form})
