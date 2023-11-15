6c5ddfecc5095e01460a2d99c39f121a76d3db56
Ritik <ritiktyagi1.618@gmail.com>
2023-11-15 15:48:12 +0530

    api v1

--- blango_blog/blango/urls.py
+++ blango_blog/blango/urls.py
@@ -21,6 +21,7 @@ from django_registration.backends.activation.views import RegistrationView
 from blango_auth.forms import BlangoRegistrationForm
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('', blog.views.index, name="index"),
     path('post/<slug:slug>/', blog.views.post_detail, name="blog-post-detail"),
@@ -30,6 +31,8 @@ urlpatterns = [
     path('accounts/register/', RegistrationView.as_view(form_class=BlangoRegistrationForm), name="django_registration_register"),
     path("accounts/", include("django_registration.backends.activation.urls")),
     path('accounts/', include('allauth.urls')),
+    path('api/v1/', include('blog.api_urls')),
+
 ]
 
 if settings.DEBUG:
