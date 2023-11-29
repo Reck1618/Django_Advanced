b06fdb11a62ae2e7cc6f3c720e97bf60102fdb19
Ritik Tyagi <ritiktyagi1.618@gmail.com>
2023-11-29 10:51:32 +0530

    started authentication

--- blango_blog/blog/api/urls.py
+++ blango_blog/blog/api/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from blog.api.views import PostList, PostDetail
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path("posts/", PostList.as_view(), name="api_post_list"),
     path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+]
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
