a226d107f0ebb94cfdb30df4298abba50ca6c009
Ritik <ritiktyagi1.618@gmail.com>
2023-12-06 17:26:29 +0530

    Finished viewsets and routers

--- blango_blog/blog/api/urls.py
+++ blango_blog/blog/api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include, re_path
-from blog.api.views import PostList, PostDetail, UserDetail
+from blog.api.views import PostViewSet, UserDetail, TagViewSet
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework.routers import DefaultRouter
 import os
+
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -12,13 +17,11 @@ schema_view = get_schema_view(
         default_version="v1",
         description="Api for Blango Blog",
     ),
-    url= f"https://localhost:8000/api/v1/",
+    url= f"http://localhost:8000/api/v1/",
     public=True,
 )
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_details"),
 ]
 
@@ -27,6 +30,7 @@ urlpatterns = format_suffix_patterns(urlpatterns)
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
+    path("", include(router.urls)),
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
--- blango_blog/blog/api/views.py
+++ blango_blog/blog/api/views.py
@@ -1,26 +1,33 @@
 
-from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer
-from blog.models import Post
+from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer, TagSerializer
+from blog.models import Post, Tag
 from blango_auth.models import User
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from blog.api.permissions import IsAuthorModifyOrReadOnly, IsAdminUserForObject
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+
+    def get_serializer_class(self):
+        if self.action in ("list", "create"):
+            return PostSerializer
+        return PostDetailSerializer
 
 class UserDetail(generics.RetrieveAPIView):
     lookup_field = "email"
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthorModifyOrReadOnly | IsAdminUserForObject]
-    queryset = Post.objects.all()
-    serializer_class = PostDetailSerializer
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+    @action(detail=True, methods=["get"], name="Posts with the tag")
+    def posts(self, request, pk=None):
+        tag = self.get_object()
+        posts = tag.posts.all()
+        serializer = PostSerializer(posts, many=True, context={"request": request})
+        return Response(serializer.data)
