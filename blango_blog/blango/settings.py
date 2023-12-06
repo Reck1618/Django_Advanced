7534bbd98970cb956806cbb7482b4b64fa71c013
Ritik <ritiktyagi1.618@gmail.com>
2023-12-06 14:26:22 +0530

    finished browsable API with swagger

--- blango_blog/blango/settings.py
+++ blango_blog/blango/settings.py
@@ -63,6 +63,7 @@ class Dev(Configuration):
         'allauth.socialaccount.providers.google',
         'rest_framework',
         'rest_framework.authtoken',
+        'drf_yasg',
     ]
 
 
@@ -75,6 +76,13 @@ class Dev(Configuration):
         'DEFAULT_PERMISSION_CLASSES': [
             'rest_framework.permissions.IsAuthenticatedOrReadOnly'
         ],
+    }
+
+    SWAGGER_SETTINGS = {
+        "SECURITY_DEFINITIONS": {
+            "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+            "Basic": {"type": "basic"},
+        },
     }
 
     MIDDLEWARE = [
---
+++ blango_blog/blog/api/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="Blango API",
+#         default_version="v1",
+#         description="Api for Blango Blog",
+#     ),
+#     url= f"https://{os.environ.get('HOSTNAME')}/api/",
+# )
