8c0ad3582d1ff7a268496a111f24c8a8cc7a35a4
Ritik Tyagi <ritiktyagi1.618@gmail.com>
2023-12-15 12:53:32 +0530

    pagination

--- blango_blog/blango/settings.py
+++ blango_blog/blango/settings.py
@@ -64,6 +64,7 @@ class Dev(Configuration):
         'rest_framework',
         'rest_framework.authtoken',
         'drf_yasg',
+        'django_filters',
     ]
 
 
@@ -88,6 +89,11 @@ class Dev(Configuration):
             "anon_burst": "10/minute",
             "user_burst": "100/minute",
         },
+        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 10,
+        'DEFAULT_FILTER_BACKENDS': [
+            'django_filters.rest_framework.DjangoFilterBackend'
+        ],
     }
 
     SWAGGER_SETTINGS = {
